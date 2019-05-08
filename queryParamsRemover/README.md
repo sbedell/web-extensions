38f1350aea61048a76b355d2209c6c64f83ae4c5
Steve B <bedell.369@osu.edu>
2019-05-08 11:18:19 -0400

    Adding queryParamsRemover and chrome extension demo files

---
+++ queryParamsRemover/README.md
@@ -0,0 +1,11 @@
+# Query Params Remover
+## Remove fbclid and utm_ query params
+
+This is working and usable right now. It still needs to be signed via addons.mozilla.org (AMO) before you can install it (permanently) locally or via the AMO site... just gonna do temporary loading for now via about:debugging. 
+
+1. Go to about:debugging
+2. Click "Load Temporary Add-on"
+3. Navigate to the folder where this extension is downloaded to
+4. Double click on `manifest.json` and it should load and show up in your add-ons list
+
+Note - Since this is just temporary loading, so it will be uninstalled every time the browser exits. 
---
+++ queryParamsRemover/queryParamsRemover.js
@@ -0,0 +1,41 @@
+/**
+ *  Remove fbclid and utm_ query params 
+ */
+
+ "use strict";
+
+function stripBadQueryParams(request) {  
+  const targetQueryParams = ["fbclid", "utm_source", "utm_medium", "utm_campaign", 
+        "utm_term", "utm_content", "utm_brand"];
+
+  let requestedUrl = new URL(request.url);
+  let match = false;
+
+  targetQueryParams.forEach(name => {
+    if (requestedUrl.searchParams.has(name)) {
+      requestedUrl.searchParams.delete(name);
+      match = true;
+    }
+  });
+
+  // return the stripped URL if a match is found, pass the URL on otherwise as normal (cancel: false)
+  return match ? {redirectUrl: requestedUrl.href} : {cancel: false};
+}
+
+/** 
+*  Event listener for onBeforeRequest (HTTP Requests)
+*  
+*  Info for the RequestFilter: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/RequestFilter
+*  Info on Types: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/ResourceType
+*  
+*/
+let apiInterface = (typeof browser != "undefined") ? browser : chrome;
+apiInterface.webRequest.onBeforeRequest.addListener(
+  stripBadQueryParams,
+  {
+    // Filters: Match all HTTP and HTTPS URLs.
+    urls: ["http://*/*", "https://*/*"],
+    types: ["main_frame", "sub_frame", "ping"]
+  },
+  ["blocking"]
+);
